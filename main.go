fe0cbc4dd6e406d19df85930fe2dd9e5f12ba9f7
Mark Olliver <mark@olliver.me.uk>
2023-04-19 21:27:37 +0100

    a few fies

---
+++ main.go
@@ -0,0 +1,15 @@
+// main.go
+package main
+
+import (
+    "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return Provider() // Use the Provider() function from the redpanda package
+		},
+	})
+}
